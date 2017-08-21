bcc42976ef50a764c3a6ff48257046d1b94ed2f6
Sdubos <sebastien.dubos@gmail.com>
2017-08-21 17:48:12 +0200

    Seb: -initial

---
+++ Antea25/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Antea25.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
