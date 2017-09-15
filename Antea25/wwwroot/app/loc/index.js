e778d0b1c7274f5ed695b60404430c86aaccfd10
Sdubos <sebastien.dubos@gmail.com>
2017-09-15 19:39:02 +0200

    from office: google map route added

--- Antea25/wwwroot/app/loc/index.js
+++ Antea25/wwwroot/app/loc/index.js
@@ -1,23 +1,50 @@
 ﻿myApp.controller('locController', function ($scope, $log, $http, $window, $timeout, apiService) {
     $scope.loadData = loadData;
     $scope.showOnMap = showOnMap;
+    $scope.removeOnMap = removeOnMap;
+    $scope.selectedGpsList = [];
     
     $scope.displayPosition = {
         gpsPositionLatitude : "48.8566",
         gpsPositionLongitude : "2.35222"
     };
 
+
+
+    $scope.ttt = {};
+    $scope.ttt.gpsPositionSelection = [];
+    $scope.ttt.gpsPositionSelection.push({ lat: "48.8566", long: "2.35222"})
+
     $scope.loadData();
 
     function loadData() {
         apiService.getLocData().then(function (response) {           
             $scope.gpsPositionList = response.data;
+            $scope.gpsPositionList[0].clicked = true;
+            for (i = 1; i < $scope.gpsPositionList.length; i++) {
+                $scope.gpsPositionList[i].clicked = false;
+            }
+
+            $scope.selectedGpsList.push($scope.gpsPositionList[0]);
+             $scope.reloadMap = 123;
+            //$scope.gpsPositionSelection = response.data;
         }, function (error) { $log.error(error.message); });
     };
 
     function showOnMap(index) {
-        $scope.displayPosition = $scope.gpsPositionList[index];
+       // $scope.displayPosition = $scope.gpsPositionList[index];
+        $scope.gpsPositionList[index].clicked = true;
+        $scope.selectedGpsList.push($scope.gpsPositionList[index]);
         $scope.reloadMap = $scope.gpsPositionList[index].gpsPositionLongitude + $scope.gpsPositionList[index].gpsPositionLatitude;
+    }
+
+    function removeOnMap(buttonIndex,item) {
+        //$scope.displayPosition = $scope.gpsPositionList[index];
+        $scope.gpsPositionList[buttonIndex].clicked = false;
+        var index = $scope.selectedGpsList.indexOf(item);
+        $scope.selectedGpsList.splice(index, 1);
+        //$scope.selectedGpsList.push($scope.gpsPositionList[index]);
+        $scope.reloadMap = $scope.selectedGpsList.length;
     }
 
 });
--- Antea25/wwwroot/app/mapDirective.js
+++ Antea25/wwwroot/app/mapDirective.js
@@ -8,13 +8,68 @@
             longitude: '=',
             latitude: '=',
             reload: '=',
+            gpslist: '=',
             time: '@',
         },
         link:  function (scope, element, attrs) {
             var map, infoWindow;
             var markers = [];
+            var waypts = [];
+            var directionsService = new google.maps.DirectionsService;
+            var directionsDisplay = new google.maps.DirectionsRenderer;
 
-            // marker
+            // init the map
+            function initMap() {
+                var mapOptions = {
+                    //center: new google.maps.LatLng(scope.latitude, scope.longitude),
+                    center: new google.maps.LatLng(scope.gpslist[0].gpsPositionLatitude, scope.gpslist[0].gpsPositionLongitude),
+                    zoom: 15,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    scrollwheel: true
+                };
+
+                map = new google.maps.Map(element[0], mapOptions);
+                directionsDisplay.setMap(map);
+
+               //We add market and way points
+                for (i = 0; i < scope.gpslist.length; i++) {
+                    setMarker(map, new google.maps.LatLng(scope.gpslist[i].gpsPositionLatitude, scope.gpslist[i].gpsPositionLongitude), 'info', scope.time.concat(" your bike was here"));
+
+                    if (i > 0 && i < scope.gpslist.length) {
+                        console.log("add ways points");
+                        waypts.push({
+                            location: new google.maps.LatLng(scope.gpslist[i].gpsPositionLatitude, scope.gpslist[i].gpsPositionLongitude),
+                            stopover: true,
+                        });
+                    } 
+                }
+
+                //we show the route
+                calcRoute(scope.gpslist[0], scope.gpslist[scope.gpslist.length - 1]);
+                
+            }
+
+            //|Add routes
+            function calcRoute(start, end) {
+                var start = new google.maps.LatLng(start.gpsPositionLatitude, start.gpsPositionLongitude);
+                var end = new google.maps.LatLng(end.gpsPositionLatitude, end.gpsPositionLongitude);
+                var request = {
+                    origin: start,
+                    destination: end,
+                    waypoints: waypts,
+                    travelMode: google.maps.TravelMode.DRIVING
+                };
+                directionsService.route(request, function (response, status) {
+                    if (status == google.maps.DirectionsStatus.OK) {
+                        directionsDisplay.setDirections(response);
+                        directionsDisplay.setMap(map);
+                    } else {
+                        alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
+                    }
+                });
+            }
+
+            //add markers
             function setMarker(map, position, title, content) {
                 var marker;
                 var markerOptions = {
@@ -41,31 +96,13 @@
                 });
             }
 
-            // init the map
-            function initMap() {
-                var mapOptions = {
-                    center: new google.maps.LatLng(scope.latitude, scope.longitude),
-                    zoom: 15,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    scrollwheel: true
-                };
-
-                map = new google.maps.Map(element[0], mapOptions);
-                //if (map === void 0) {
-                //    map = new google.maps.Map(element[0], mapOptions);
-                //}
-                setMarker(map, new google.maps.LatLng(scope.latitude, scope.longitude), 'info', scope.time.concat(" your bike is here"));
-            }
-
-          
-
-            // show the map and place some markers
-            
-            initMap();
-
-            scope.$watch("reload", function () {
-                initMap();
-                
+            //Show the map when data loaded
+            scope.$watch("reload", function () {   
+                if (scope.gpslist.length>0) {
+                    initMap();
+                    waypts = [];
+                    markers = [];
+                }
             });
 
         }
